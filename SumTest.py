ddc6dd4216b1cdcc05eca9b5af1504bca36a7fd3
Paul Lennon <lennon.paul@gmail.com>
2019-02-27 16:47:49 +0000

    updated

--- SumTest.py
+++ SumTest.py
@@ -3,8 +3,9 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
-
-
+        self.assertEqual(True, True)
+#comment
+    def test_something2(self):
+        self.assertEqual(True, True)
 if __name__ == '__main__':
     unittest.main()
